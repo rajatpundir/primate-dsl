1ad73a96a8c312de98eb72da66e6cbc1677a1a17
Rajat Pundir <rajat.pundir@pibity.com>
2021-01-12 21:11:40 +0530

    Added copyright

--- src/Entities/Function/FunctionOutputType.hs
+++ src/Entities/Function/FunctionOutputType.hs
@@ -1,3 +1,11 @@
+--
+-- Copyright (C) 2021 Rajat Pundir <rajatpundir13@gmail.com> - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- THIS IS UNPUBLISHED PROPRIETARY CODE OF RAJAT PUNDIR <rajatpundir13@gmail.com>
+-- The copyright notice above does not evidence any actual or intended publication of such source code.
+--
+
 {-# LANGUAGE TemplateHaskell #-}
 
 module Entities.Function.FunctionOutputType(FunctionOutputType(..)) where
